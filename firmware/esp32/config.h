d7fe04a286c255cbcb287f34d678d08892783ccf
Gopi C K <131648444+gopi-c-k@users.noreply.github.com>
2026-01-21 18:34:22 +0530

    Add configuration header for ESP32 firmware

---
+++ firmware/esp32/config.h
@@ -0,0 +1,18 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Motor Pins
+#define ENA 25
+#define IN1 26
+#define IN2 27
+
+// FSR Pins
+#define FSR1 34
+#define FSR2 35
+
+// Thresholds
+#define FSR_THRESHOLD 45.0
+#define MEDIUM_MOTION_THRESHOLD 0.8
+#define DANGER_MOTION_THRESHOLD 1.8
+
+#endif
